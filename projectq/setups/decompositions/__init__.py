e4206c4c81e809c2b19512b86b4777eeff5345bf
Kevin J. Sung <kevinjefferysung@gmail.com>
2018-01-10 15:56:05 -0500

    add cry decomposition to default

--- projectq/setups/decompositions/__init__.py
+++ projectq/setups/decompositions/__init__.py
@@ -14,6 +14,7 @@
 
 from . import (arb1qubit2rzandry,
                barrier,
+               cry2crzandhadamardandsgate,
                carb1qubit2cnotrzandry,
                crz2cxandrz,
                cnu2toffoliandcu,
@@ -32,6 +33,7 @@ all_defined_decomposition_rules = [
     rule
     for module in [arb1qubit2rzandry,
                    barrier,
+                   cry2crzandhadamardandsgate,
                    carb1qubit2cnotrzandry,
                    crz2cxandrz,
                    cnu2toffoliandcu,
